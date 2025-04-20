58112b4683915cf418dbb6fa4c01c2a4adc90395
utkucnay <utkucnay@gmail.com>
2025-04-20 12:15:41 +0300

    Slab and Tests

--- Walker/src/Memory/USlabAllocater.cc
+++ Walker/src/Memory/USlabAllocater.cc
@@ -3,36 +3,46 @@
 namespace wkr::mem {
 
 int RoundToUpperPowerOfTwo(int n) {
-    if (n <= 0) return -1;
+  if (n <= 0)
+    return -1;
 
-    if ((n & (n - 1)) == 0) return n;
+  if ((n & (n - 1)) == 0)
+    return n;
 
-    int lowerPower = __builtin_ctz(~n);
-    int upperPower = 1 << lowerPower;
+  int lowerPower = __builtin_ctz(~n);
+  int upperPower = 1 << lowerPower;
 
-    return upperPower;
+  return upperPower;
 }
 
 void* USlabAllocator::Allocate(i32 size) {
   size = RoundToUpperPowerOfTwo(size);
 
   FSlabCacheGroup slabCacheGroup = m_SlabSizeMap[size];
-  FSlabCache slabCache;
 
-  if (!slabCacheGroup.PartialSlabCache.empty()) {
-    slabCache = slabCacheGroup.PartialSlabCache.front();
-  } else if (!slabCacheGroup.EmptySlabCache.empty()) {
-    slabCache = slabCacheGroup.EmptySlabCache.front();
-  } else {
-    slabCache = FSlabCache{
-        .SlabObjectFlag = 0,
-        .SlabObjectCount = 16,
-        .MemoryAdrress = m_MemoryPool.Allocate(size * 16),
-    };
-
-    slabCacheGroup.PartialSlabCache.push_back(slabCache);
+  if (slabCacheGroup.AvailableSlabCache == nullptr) {
+    slabCacheGroup.AvailableSlabCache = new FSlabCache(
+        m_MemoryPool.Allocate(size * FSlabCache::kObjectSize), size);
   }
 
+  FSlabCache* slabCache = slabCacheGroup.AvailableSlabCache;
+  FSlabObject& slabObject = slabCache->SlabObjects[slabCache->SlabObjectIndex];
+
+  slabObject.isUsed = true;
+  slabCache->SlabObjectIndex = slabObject.nextSlabObjectIndex;
+
+  if (slabObject.nextSlabObjectIndex == -1) {
+    slabCacheGroup.AvailableSlabCache = slabCache->nextSlabCache;
+
+    FSlabCache* swapSlabCache = slabCacheGroup.FullSlabCache;
+    slabCacheGroup.FullSlabCache = slabCache;
+    slabCache->nextSlabCache = swapSlabCache;
+  }
+
+  return reinterpret_cast<void*>(slabObject.MemoryAdress);
+}
+
+void USlabAllocator::Deallocate(void* ptr) {
 
 }
 
